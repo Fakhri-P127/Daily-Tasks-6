3ef519efdacf1c6ffaf0d2ec9584cdadb41c043a
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-19 23:28:35 +0400

    added Array-Sort folder

---
+++ Array-Sort/Array-Sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Array_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 51, 212, 6, 0, 12, -241, -21, 97, 1 };
+            Array.Sort(numbers);
+            foreach (int index in numbers)
+            {
+                Console.Write($"{index} ");
+            }
+        }
+    }
+}
